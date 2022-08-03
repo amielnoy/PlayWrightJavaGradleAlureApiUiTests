507f8c4c306acd3103aff3d99a73d5ba221e8b25
amiel <amielnoy@gmail.com>
2022-08-03 21:03:00 +0300

    Add gradle Add allure reporting

--- src/test/java/conduitTests/E2E/SignInTest.java
+++ src/test/java/conduitTests/E2E/SignInTest.java
@@ -4,15 +4,23 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 
 import POM.HomePage;
 import POM.SignInPage;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.params.ParameterizedTest;
+//import org.junit.jupiter.params.provider.CsvSource;
+import fixtures.TestFixtures;
+import org.junit.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import fixtures.TestFixtures;
 
 public class SignInTest extends TestFixtures {
 
-    @ParameterizedTest
-    @CsvSource({"interview@start.com,password"})
-    void shouldSignIn(String email, String password) {
+
+    //@ParameterizedTest
+    @Test
+    //@CsvSource({"interview@start.com,password"})
+    public void shouldSignIn() {
+        String email="interview@start.com";
+        String password="password";
         new SignInPage(page).navigate().signIn(email, password);
         assertThat(new HomePage(page).userProfileLink).isVisible();
     }
